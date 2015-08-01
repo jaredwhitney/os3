ac17372c7fd0875e6cf5006fe3582e41acd5ccf3
jared whitney <jaredwhitney3@gmail.com>
2015-08-01 15:28:20 -0400

    [Broken] Begin work on new updateScreen

--- Modules/dolphin/Dolphin.asm
+++ Modules/dolphin/Dolphin.asm
@@ -67,78 +67,141 @@ ret
 	popa
 	ret
 
-Dolphin.updateScreen :
-pusha
-	cmp byte [Dolphin_WAIT_FLAG], 0xFF
-		je Dolphin.updateScreen.ret
-	push word [Dolphin.currentWindow]
-			; 	If an exception occurs, blame Dolphin.	;
-	mov bl, Manager.CONTROL_DOLPHIN
-	mov [os.mlloc], bl
-			; has active window changed? if so, every window with a lower depth than its previous depth should have their depth incremented by 1, the new active window should have its depthset to 0.
+; Dolphin.updateScreen :
+; pusha
+	; cmp byte [Dolphin_WAIT_FLAG], 0xFF
+		; je Dolphin.updateScreen.ret
+	; push word [Dolphin.currentWindow]
+			; ; 	If an exception occurs, blame Dolphin.	;
+	; mov bl, Manager.CONTROL_DOLPHIN
+	; mov [os.mlloc], bl
+			; ; has active window changed? if so, every window with a lower depth than its previous depth should have their depth incremented by 1, the new active window should have its depthset to 0.
 
-;	Draw windows in here!
-	mov ebx, 0x0
-	mov ecx, 0x0
-	Dolphin.updateScreen.checkWindow :
-						; loop through each window, find the one with the highest depth, then highest depth lower than that depth, etc. (run through all of the windows in order of decreasing depth)
-		cmp ebx, 0x3C	; num of windows * 4 bytes per windowStruct location
-			jge Dolphin.doneDrawingWindows
+; ;	Draw windows in here!
+	; mov ebx, 0x0
+	; mov ecx, 0x0
+	; Dolphin.updateScreen.checkWindow :
+						; ; loop through each window, find the one with the highest depth, then highest depth lower than that depth, etc. (run through all of the windows in order of decreasing depth)
+		; cmp ebx, 0x3C	; num of windows * 4 bytes per windowStruct location
+			; jge Dolphin.doneDrawingWindows
 			
-		call Dolphin.windowExists
-		add ebx, 4
-		cmp eax, 0x0
-			je Dolphin.updateScreen.checkWindow
+		; call Dolphin.windowExists
+		; add ebx, 4
+		; cmp eax, 0x0
+			; je Dolphin.updateScreen.checkWindow
 			
-		push ebx
-		sub ebx, 4
+		; push ebx
+		; sub ebx, 4
 		
-		mov [Dolphin.currentWindow], ebx
-		add ebx, Dolphin.windowStructs
-		mov edx, [ebx]
-		push edx
+		; mov [Dolphin.currentWindow], ebx
+		; add ebx, Dolphin.windowStructs
+		; mov edx, [ebx]
+		; push edx
 		
-		mov [atwstorX], ax
+		; mov [atwstorX], ax
 		
-		mov [atwstor2X], ax
+		; mov [atwstor2X], ax
 		
-		mov eax, [edx+14]	; windowbuffer
+		; mov eax, [edx+14]	; windowbuffer
 		
-		xor ecx, ecx
-		mov cx, [edx+4]	; width
-		mov dx, [edx+6]	; height
-		and edx, 0xFFFF
+		; xor ecx, ecx
+		; mov cx, [edx+4]	; width
+		; mov dx, [edx+6]	; height
+		; and edx, 0xFFFF
 
-		mov ebx, eax
-		call Dolphin.drawBorder
-		call Dolphin.drawTitle
+		; mov ebx, eax
+		; call Dolphin.drawBorder
+		; call Dolphin.drawTitle
 
-		mov ebx, [Graphics.SCREEN_MEMPOS]
+		; mov ebx, [Graphics.SCREEN_MEMPOS]
 		
-		pop edx
-		push eax
-		xor eax, eax
-		mov ax, [edx+10]	; ypos
-		imul eax, [Graphics.SCREEN_WIDTH]
+		; pop edx
+		; push eax
+		; xor eax, eax
+		; mov ax, [edx+10]	; ypos
+		; imul eax, [Graphics.SCREEN_WIDTH]
 			
-		add ebx, eax	; add ypos
-		xor eax, eax
-		mov ax, [edx+8]
-		add ebx, eax	; add xpos
-		pop eax
-		mov dx, [edx+6]	; height back
+		; add ebx, eax	; add ypos
+		; xor eax, eax
+		; mov ax, [edx+8]
+		; add ebx, eax	; add xpos
+		; pop eax
+		; mov dx, [edx+6]	; height back
 
-		call Image.copy
+		; call Image.copy
 		
-		pop ebx
-		jmp Dolphin.updateScreen.checkWindow
+		; pop ebx
+		; jmp Dolphin.updateScreen.checkWindow
 		
-	Dolphin.doneDrawingWindows :
-	pop word [Dolphin.currentWindow]
+	; Dolphin.doneDrawingWindows :
+	; pop word [Dolphin.currentWindow]
 	
-Dolphin.updateScreen.ret :
+; Dolphin.updateScreen.ret :
+; popa
+; ret
+
+; *** NEW SCREEN UPDATE CODE ***
+
+Dolphin.updateScreenNew :
+pusha
+;
+; for each window
+; 	does it need rects drawn?
+;		yes:
+;			window is on top:
+;				updateRects
+;			window is not on top:
+;				figure out occlusion somehow...
+;	window has been moved/resized:
+;		updateAll = true
+;
+; if updateAll
+;	redraw background
+;	for each window
+;		redraw winBuffer to screen
+;
 popa
 ret
+
+Dolphin.updateScreenNew.updateRects :
+pusha
+mov [espStor], esp	; store current stack
+mov [ebpStor], ebp
+
+pusha			; store window's position
+	xor eax, eax
+	mov bl, [Window.Y_POS]
+	call Dolphin.getAttribWord
+	mov ecx, eax
+	mov bl, [Window.WIDTH]
+	call Dolphin.getAttribWord
+	imul ecx, eax
+	mov bl, [Window.X_POS]
+	call Dolphin.getAttribWord
+	add ecx, eax
+	mov [cWinPos], ecx
+popa
+
+mov ebp, [uRectsBase]	; load rects stack
+mov esp, [uRectsEnd]
+
+Dolphin.updateNew.cRectsLoop :	; update rects
+pop eax	; eg 0= ul of win in winB
+mov ebx, [Graphics.SCREEN_MEMPOS]	; ul of screen
+add ebx, [cWinPos]	; ul of win
+add ebx, eax	; where to be in win
+mov cx, 5
+mov dx, 7
+call Image.copy
+cmp ebp, esp
+	jne Dolphin.updateNew.cRectsLoop
+
+mov ebp, [ebpStor]	; load old stack
+mov esp, [espStor]
+popa
+ret
+
+; *** END NEW SCREEN UPDATE CODE ***
 
 Dolphin.uUpdate :	; currentWindow is the window
 pusha
