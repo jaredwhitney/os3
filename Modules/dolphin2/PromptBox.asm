3a337891333fcc5b11232c5b693779f84e28b1b8
jared whitney <jaredwhitney3@gmail.com>
2016-05-05 23:39:10 -0400

    Make PromptBox.PromptForString work

--- Modules/dolphin2/PromptBox.asm
+++ Modules/dolphin2/PromptBox.asm
@@ -1,7 +1,7 @@
-PromptBox.WaitForResponse :	; String name, String prompt, String callback
+PromptBox.PromptForString :	; String title, String message, String callback
 	enter 0, 0
-		pusha
-		push dword [ebp+12]
+		;pusha
+		push dword [ebp+16]
 		push dword 0*4
 		push dword 0
 		push dword 300*4
@@ -9,7 +9,7 @@ PromptBox.WaitForResponse :	; String name, String prompt, String callback
 		call Dolphin2.makeWindow
 		mov dword [ecx+Grouping_backingColor], 0xFF03010C
 		mov ebx, ecx
-		push dword [ebp+8]
+		push dword [ebp+12]
 		push dword 10*4
 		push dword 10
 		push dword 300*4
@@ -29,9 +29,9 @@ PromptBox.WaitForResponse :	; String name, String prompt, String callback
 		mov dword [PromptBox.hasResponse], false
 		mov edx, [ecx+Textarea_text]
 		mov [Dolphin2.focusedComponent], ecx
-		mov [PromptBox.str], edx
+		mov [PromptBox.response], edx
 		push dword PromptBox.STR_DONE
-		push dword [ebp+16]
+		push dword [ebp+8]
 		push dword 10*4
 		push dword 20+2*FONTHEIGHT
 		push dword 4*FONTWIDTH*4
@@ -39,17 +39,14 @@ PromptBox.WaitForResponse :	; String name, String prompt, String callback
 		call Button.Create
 		mov eax, ecx
 		call Grouping.Add
-		popa
+		;popa
 	leave
 	ret 12
-PromptBox.GotResponse :
-	mov dword [PromptBox.hasResponse], true
-	ret
 PromptBox.hasResponse :
 	dd 0x0
 PromptBox.window :
 	dd 0x0
-PromptBox.str :
+PromptBox.response :
 	dd 0x0
 PromptBox.STR_DONE :
 	db "DONE", 0
