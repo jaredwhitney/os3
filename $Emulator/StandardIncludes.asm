add300eb803676e3c66bd689f5d65518aecbb148
jared whitney <jaredwhitney3@gmail.com>
2016-05-23 15:26:48 -0400

    Clean and comment files

--- $Emulator/StandardIncludes.asm
+++ $Emulator/StandardIncludes.asm
@@ -1,94 +1,159 @@
-%include "..\boot\betterRealMode.asm"
-%include "..\boot\IDTmain.asm"
-%include "..\boot\detection.asm"
-%include "..\Drivers\GFX\Generic_Display.asm"
-%include "..\Drivers\PS2\PS_2.asm"
-%include "..\Drivers\PS2\Generic_Mouse.asm"
-%include "..\Drivers\PS2\Generic_Keyboard.asm"
+; BOOT CRITICAL
 
-%include "..\modules\dolphin\Dolphin.asm"
-%include "..\modules\dolphin2\Dolphin2.asm"
-%include "..\modules\dolphin2\PromptBox.asm"
-%include "..\modules\dolphin\WindowTools.asm"
-%include "..\modules\dolphin\Update_Text.asm"
+	;  Stage2 Required Includes	;
+		%include "..\boot\betterRealMode.asm"
+		%include "..\boot\IDTmain.asm"
+		%include "..\Drivers\PS2\PS_2.asm"	; init should be dealt with in kernel, not s2!
+		
+	;  BootCritical Signature	;
+		stage2_minloadSig : dd 0xDEADF154
 
-%include "..\modules\console\iConsole.asm"
-%include "..\modules\console\iConsole2.asm"
-%include "..\modules\textEditor\TextEditor.asm"
-%include "..\modules\imageEditor\ImageEditor.asm"
-%include "..\modules\video\Video.asm"
-%include "..\modules\xlib\int30.asm"
 
-%include "..\Graphics\Image\Tools.asm"
-%include "..\Graphics\Image\TransparencyUtils.asm"
-%include "..\Graphics\TextHandler.asm"
-%include "..\Graphics\RenderTextThing.asm"
-%include "..\Graphics\TextArea.asm"
-%include "..\Graphics\Image.asm"
-%include "..\Graphics\Grouping.asm"
-%include "..\Graphics\Button.asm"
-%include "..\Graphics\WindowGrouping.asm"
-%include "..\Graphics\GroupingScrollable.asm"
-%include "..\Graphics\SelectionPanel.asm"
-%include "..\Graphics\Font\SysDefault.asm"
+; KERNEL CRITICAL
 
-;  3GXlib Includes  ;
-	%include "..\Graphics\3GXlib\L3gxImage.asm"
-	%include "..\Graphics\3GXlib\DrawRect.asm"
-	%include "..\Graphics\3GXlib\ClearImage.asm"
-	%include "..\Graphics\3GXlib\DrawImage.asm"
+	;  Memory Check Includes	;
+		%include "..\boot\detection.asm"
+		
+	;  PS/2 Driver Includes	;
+		%include "..\Drivers\PS2\Generic_Mouse.asm"
+		%include "..\Drivers\PS2\Generic_Keyboard.asm"
 
-%include "..\Drivers\HDD\rmATA.asm"
-%include "..\Drivers\HDD\ATA_DETECT.asm"
-%include "..\Drivers\HDD\AHCI_COMMANDS.asm"
-%include "..\Drivers\HDD\AHCI_DMAWRITE.asm"
 
-%include "..\Drivers\PCI\Generic_ComponentInterface.asm"
+; GRAPHICS
 
-%include "..\Tools\System\Time.asm"
-%include "..\Tools\System\Guppy.asm"
-;%include "..\Tools\System\Minnow.asm"
-%include "..\Tools\System\Minnow4.asm"
-%include "..\Tools\System\Minnow3.asm"
-%include "..\Tools\System\Minnow2.asm"
-%include "..\Tools\System\GetValue.asm"
-;%include "..\Tools\System\TaskSwapHandler.asm"
+	;  Graphics Driver Includes	;
+		%include "..\Drivers\GFX\Generic_Display.asm"
+		
+	;  3GXlib Includes  ;
+		%include "..\Graphics\3GXlib\L3gxImage.asm"
+		%include "..\Graphics\3GXlib\DrawRect.asm"
+		%include "..\Graphics\3GXlib\ClearImage.asm"
+		%include "..\Graphics\3GXlib\DrawImage.asm"
+	
+	;  Text and Font Includes	;
+		%include "..\Graphics\TextHandler.asm"
+		%include "..\Graphics\Font\SysDefault.asm"	
+	
+	;  Dolphin Includes	;
+		%include "..\modules\dolphin\Dolphin.asm"	; depricated
+		%include "..\Meta\Graphics\Window.asm"	; depricated
+		%include "..\modules\dolphin2\Dolphin2.asm"
+		%include "..\modules\dolphin2\PromptBox.asm"
+		%include "..\modules\dolphin\WindowTools.asm"
+		%include "..\modules\dolphin\Update_Text.asm"
+	
+	;  Component Includes	;
+		%include "..\Graphics\RenderTextThing.asm"
+		%include "..\Graphics\TextArea.asm"
+		%include "..\Graphics\Image.asm"
+		%include "..\Graphics\Grouping.asm"
+		%include "..\Graphics\Button.asm"
+		%include "..\Graphics\WindowGrouping.asm"
+		%include "..\Graphics\GroupingScrollable.asm"
+		%include "..\Graphics\SelectionPanel.asm"
 
-%include "..\Tools\Utility\Clock.asm"
 
-%include "..\Tools\Security\Manager.asm"
-%include "..\Tools\Security\HaltScreen.asm"
+; USER SIDE
+		
+	;  iConsole Includes	;
+		%include "..\OrcaHLL\iConsole.asm"	; depricated
+		%include "..\modules\console\iConsole.asm"	; depricated
+		%include "..\modules\console\iConsole2.asm"
+	
+	;  Module Includes	;
+		%include "..\modules\textEditor\TextEditor.asm"
+		%include "..\modules\imageEditor\ImageEditor.asm"
+		%include "..\modules\video\Video.asm"
+	
+	;  Program Loader Includes	;
+		%include "..\kernel\ProgramLoader.asm"	; depricated in part
+	
+	;  Syscall Includes	;
+		%include "..\modules\xlib\int30.asm"
 
-%include "..\Tools\String\General.asm"
-%include "..\Tools\String\EqualityTests.asm"
-%include "..\Tools\Number\BCDconversions.asm"
 
-%include "..\OrcaHLL\iConsole.asm"
+; DRIVER CRITICAL
+		
+	;  PCI Driver Includes	;
+		%include "..\Drivers\PCI\Generic_ComponentInterface.asm"
+		; %include "..\Drivers\PCI\PCI_LookupTables.asm"
 
-%include "..\kernel\ProgramLoader.asm"
-%include "..\kernel\Paging.asm"
 
-%include "..\debug\TextMode.asm"
-%include "..\debug\MemoryCheck.asm"
-%include "..\debug\print.asm"	; depricated
+; HARD-DRIVE AND FILESYSTEM
+		
+	;  HDD Driver Includes	;
+		%include "..\Drivers\HDD\rmATA.asm"
+		%include "..\Drivers\HDD\ATA_DETECT.asm"
+		%include "..\Drivers\HDD\AHCI_COMMANDS.asm"
+		%include "..\Drivers\HDD\AHCI_DMAWRITE.asm"
+	
+	;  MinnowFS Includes	;
+		%include "..\Tools\System\Minnow2.asm"	; depricated
+		%include "..\Tools\System\Minnow3.asm"	; depricated
+		%include "..\Tools\System\Minnow4.asm"
 
-%include "..\Meta\Graphics\Window.asm"
-%include "..\Meta\Buffer.asm"
 
-;%include "..\Drivers\PCI\PCI_LookupTables.asm"
+; SYSTEM MISC
+	
+	;  System Tool Includes	;
+		%include "..\Tools\System\Time.asm"
+		%include "..\Tools\System\Guppy.asm"
+		%include "..\Tools\System\GetValue.asm"
+		; %include "..\Tools\System\TaskSwapHandler.asm"
+	
+	;  System Utility Includes	;
+		%include "..\Tools\Utility\Clock.asm"	; depricated in part
+	
+	;  System Security Includes	;
+		%include "..\Tools\Security\Manager.asm"
+		%include "..\Tools\Security\HaltScreen.asm"
+		
+	;  System Debug Includes	;
+		%include "..\debug\TextMode.asm"
+		%include "..\debug\MemoryCheck.asm"
+		%include "..\debug\print.asm"	; depricated
 
-%include "..\Drivers\USB\EHCI.asm"
 
-Catfish.notify:
-ret
-View.file:
-ret
-TRUE equ 0xFF
-true equ 0xFF
-FALSE equ 0x00
-false equ 0x00
-null equ 0x0
-nullptr :
-	dq 0
-endstr equ 0x0
-WHITE equ 0xFF
+; UTILITY
+	
+	;  Graphics Utility Includes	;
+		%include "..\Graphics\Image\Tools.asm"
+		%include "..\Graphics\Image\TransparencyUtils.asm"
+	
+	;  String Utility Includes	;
+		%include "..\Tools\String\General.asm"
+		%include "..\Tools\String\EqualityTests.asm"
+	
+	;  Number Utility Includes	;
+		%include "..\Tools\Number\BCDconversions.asm"
+		
+	;  Buffer Utility Includes	;
+		%include "..\Meta\Buffer.asm"
+
+
+; MISC
+
+	;  Paging Includes	;
+		%include "..\kernel\Paging.asm"	; unimplemented
+	
+	;  EHCI Driver Includes	;
+		%include "..\Drivers\USB\EHCI.asm"	; unimplemented
+
+
+; DEFINITIONS
+
+	; Truthy / Falsey
+		TRUE equ 0xFF
+		true equ 0xFF
+		FALSE equ 0x00
+		false equ 0x00
+	
+	; Null
+		null equ 0x0
+		nullptr : dq 0
+	
+	; String
+		endstr equ 0x0
+	
+	; 255-Color Palette
+		WHITE equ 0xFF
